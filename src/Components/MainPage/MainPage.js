c7f78a51db26862db780478316dde3d960a17d4c
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-11-24 12:35:57 -0300

    exercicios finalizados no dia 24/11/2021

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,27 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MainContainer, Form, Input } from './styles'
+import { useForm } from "../../hooks/useForm"
 
 function MainPage() {
-  const [nome, setNome] = useState("")
-  const [idade, setIdade] = useState("")
-  const [email, setEmail] = useState("")
+  // // const [nome, setNome] = useState("")
+  // // const [idade, setIdade] = useState("")
+  // // const [email, setEmail] = useState("")
+  // const [formulario, setFormulario] = useState({ nome: "", idade: "", email: "" })
 
-  const onChangeNome = (event) => {
-    setNome(event.target.value)
-  }
+  // console.log(formulario)
 
-  const onChangeIdade = (event) => {
-    setIdade(event.target.value)
-  }
+  // // const onChangeNome = (event) => {
+  // //   setNome(event.target.value)
+  // // }
 
-  const onChangeEmail = (event) => {
-    setEmail(event.target.value)
-  }
+  // // const onChangeIdade = (event) => {
+  // //   setIdade(event.target.value)
+  // // }
+
+  // // const onChangeEmail = (event) => {
+  // //   setEmail(event.target.value)
+  // // }
+
+  // // este onChange sera enviado a pasta hooks.
+
+  // const onChange = (event) => {
+  //   // const name = event.target.name
+  //   // const valor = event.target.value
+  //   const {name, value} = event.target
+  //   setFormulario({...formulario,[name]:value})
+  // }
+  const [formulario, onChange] = useForm({ nome: "", profissao:"", idade: "", email: "" })
+  useEffect(() => { }, [])
+  useState("")
+
 
   const handleClick = (event) => {
     event.preventDefault()
 
-    console.log(`nome: ${nome}, idade: ${idade}, e-mail: ${email} `)
+    console.log(`nome: ${formulario.nome}, profissao:${formulario.profissao} idade: ${formulario.idade}, e-mail: ${formulario.email} `)
   }
 
   return (
@@ -30,28 +47,47 @@ function MainPage() {
 
       <Form onSubmit={handleClick}>
         <label htmlFor="nome">Nome:</label>
-        <Input 
+        <Input
+          name="nome"
           id="nome"
-          value={nome}
-          onChange={onChangeNome}
+          value={formulario.nome}
+          onChange={onChange}
+          required
+          type="text"
         />
 
         <label htmlFor="idade">Idade:</label>
-        <Input 
+        <Input
+          name="idade"
           id="idade"
-          value={idade}
-          onChange={onChangeIdade}
+          value={formulario.idade}
+          onChange={onChange}
+          type="number"
+          required
+        />
+
+        <label htmlFor="profissao">Profissao:</label>
+        <Input
+          name="profissao"
+          id="profissao"
+          value={formulario.profissao}
+          onChange={onChange}
+          required
         />
 
         <label htmlFor="email">E-mail:</label>
-        <Input 
+        <Input
+          name="email"
           id="email"
-          value={email}
-          onChange={onChangeEmail}
+          value={formulario.email}
+          onChange={onChange}
+          type="email"
+          required
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
         />
-        
-        
-      <button type="submit">Enviar dados</button>
+
+
+        <button type="submit">Enviar dados</button>
       </Form>
     </MainContainer>
   )
